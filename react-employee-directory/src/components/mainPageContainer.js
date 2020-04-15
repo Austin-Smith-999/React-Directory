4f7fdcb2a08e900c1cb1c93b89a5d7e6514a83c1
Austin Smith <austin.smith.ads@gmail.com>
2020-04-14 22:25:39 -0400

    newest

---
+++ react-employee-directory/src/components/mainPageContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import mainPageContainer from "./components/mainPageContainer";
+
+function App() {
+  return <mainPageContainer />;
+}
+
+
+export default App;
