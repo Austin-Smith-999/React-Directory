73a2e3ec6e18dfd3bfedbdae060d533522cb9c6a
Austin Smith <austin.smith.ads@gmail.com>
2020-04-16 15:07:06 -0400

    searchbox centered and has 1em margin from top

--- react-employee-directory/src/components/NavBar.js
+++ react-employee-directory/src/components/NavBar.js
@@ -8,11 +8,11 @@ function NavBar(props) {
             <h1 className="titleText">Employee Directory</h1>
             <h4 className="titleText">Look for employees using search box</h4>
         </nav>
-        <form action="">
+        
         {/* <input className="alignCenter" type="search" placeholder="Search" aria-label="Search"></input>
          */}
-         <input className="alignCenter" type="search" placeholder="Search"/>
-        </form>
+         <input className="searchBox" type="search" placeholder="Search"/>
+        
         </div>
 
 
