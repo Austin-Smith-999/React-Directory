71225549997423d96bdd6aca3c23760ae9d3729c
Austin Smith <austin.smith.ads@gmail.com>
2020-04-16 11:45:31 -0400

    fixed readme in react app, some minor changes

--- react-employee-directory/src/components/NavBar.js
+++ react-employee-directory/src/components/NavBar.js
@@ -4,8 +4,8 @@ import './App.css';
 function NavBar(props) {
     return (
         <nav className="navBar">
-            <h1>Employee Directory</h1>
-            <h4>Look for employees using search box</h4>
+            {/* <h1>Employee Directory</h1>
+            <h4>Look for employees using search box</h4> */}
         </nav>
 
 
