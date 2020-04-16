03d5e331f00ccb40a1469020c26d944087a8615a
Austin Smith <austin.smith.ads@gmail.com>
2020-04-16 15:01:23 -0400

    made it render by changing ./app.css to ../app.css

--- react-employee-directory/src/components/NavBar.js
+++ react-employee-directory/src/components/NavBar.js
@@ -1,13 +1,19 @@
 import React from "react";
-import './App.css';
+import '../App.css';
 
 function NavBar(props) {
     return (
+        <div>
         <nav className="navBar">
-            {/* <h1>Employee Directory</h1>
-            <h4>Look for employees using search box</h4> */}
+            <h1 className="titleText">Employee Directory</h1>
+            <h4 className="titleText">Look for employees using search box</h4>
         </nav>
-
+        <form action="">
+        {/* <input className="alignCenter" type="search" placeholder="Search" aria-label="Search"></input>
+         */}
+         <input className="alignCenter" type="search" placeholder="Search"/>
+        </form>
+        </div>
 
 
     );
