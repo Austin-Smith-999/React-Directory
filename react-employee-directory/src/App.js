ab274c8eab4c6c3a09bed3491f9776e237b8b34f
Austin Smith <austin.smith.ads@gmail.com>
2020-04-20 17:05:36 -0400

    added components for tables of info and added to main container

--- react-employee-directory/src/App.js
+++ react-employee-directory/src/App.js
@@ -2,12 +2,29 @@ import React from 'react';
 import './App.css';
 // import mainPageContainer from "./components/mainpageContainer";
 import NavBar from "./components/NavBar";
+import tablesOfPeopleContainer from './components/tablesOfPeopleContainer';
 
 function App() {
-  return <NavBar />;
+  return ( 
+    <div>
+  <NavBar />
+  <tablesOfPeopleContainer />
+  </div>
+
+          );
 }
-// render() {
-//   return( <NavBar />
-//   )};
+        
 
 export default App;
+
+// function App() {
+//   return (
+//     <div>
+//       <Navbar />
+//       <Header />
+//       <Section />
+//     </div>
+//   );
+// }
+
+// export default App;
--- react-employee-directory/src/components/tablesOfPeopleContainer.js
+++ react-employee-directory/src/components/tablesOfPeopleContainer.js
@@ -1,26 +1,98 @@
-import React from "react";
+import React, { Component } from "react";
 import '../App.css';
+import NavBar from "./NavBar";
+import Col from "./column";
+import API from "../utils/API";
+import Row from "./Row";
+import Card from "./card";
+import Container from "./Container";
 
 class tablesOfPeopleContainer extends Component {
     state = {
-        searchBox: ""
-    //   currentPage: "Home"
+		search: "",
+		results: [],
     };
   
     // handlePageChange = page => {
     //   this.setState({ currentPage: page });
     // };
 
-
+    componentDidMount() {
+      this.searchEmployees();
+    }
   
+    searchEmployees = () => {
+      API.search()
+        // res is the data coming back from the axios call;
+        .then((res) => this.setState({ results: res.data.results }))
+        .catch((err) => console.log(err));
+    };
+  
+
     render() {
+      //
+      if (this.state.results.length === 0) return <p>Loading...</p>;
+  
       return (
-        <div>
-          <NavTabs
-            currentPage={this.state.currentPage}
-            handlePageChange={this.handlePageChange}
-          />
-        </div>
+        <Container>
+          <Card className="justify-content-center">
+            <Row heading="Your Awesome Company!!!">
+              <Col size="md-3">
+                <button
+                  className="btn btn-light btn-sm"
+                  onClick={this.handleSort}
+                  disabled
+                >
+                  HEAD-SHOT
+                </button>
+              </Col>
+              <Col size="md-3" className="btn btn-light btn-sm">
+                <button
+                  className="btn btn-light btn-sm"
+                  onClick={this.handleSort}
+                >
+                  NAME
+                </button>
+              </Col>
+              <Col size="md-3">
+                <button
+                  className="btn btn-light btn-sm"
+                  onClick={this.handleSort}
+                  disabled
+                >
+                  CELL
+                </button>
+              </Col>
+              <Col size="md-3">
+                <button
+                  className="btn btn-light btn-sm"
+                  onClick={this.handleSort}
+                  disabled
+                >
+                  EMAIL
+                </button>
+              </Col>
+            </Row>
+          </Card>
+          {this.state.results.map((employee) => {
+            console.log(employee);
+            return (
+              // outermost component in a loop needs a key value PROP.
+              <Card key={employee.id.value}>
+                <Row>
+                  <Col size="md-3">
+                    <img src={employee.picture.medium} />
+                  </Col>
+                  <Col size="md-3">
+                    {employee.name.first} {employee.name.last}
+                  </Col>
+                  <Col size="md-3">{employee.cell}</Col>
+                  <Col size="md-3">{employee.email}</Col>
+                </Row>
+              </Card>
+            );
+          })}
+        </Container>
       );
     }
   }
